abb7e5f87debe400cde37398fb32189ad6244117
LeonardSuli <leonardsuli1991@gmail.com>
2024-01-15 22:10:26 +0100

    prove to push number in numFound

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 
 // 1 - Visualizzare in pagina 5 numeri casuali. 
-// 2 - Da lì parte un timer di 10 secondi. 
-// 3 - Dopo 10 secondi i numeri scompaiono 
+// 2 - Da lì parte un timer di 5 secondi. 
+// 3 - Dopo 5 secondi i numeri scompaiono 
 // 4 - e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt(). 
 // 5 - Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
@@ -13,6 +13,25 @@
 const numbers = document.querySelector('h1')
 
 
+
+const numCasuali = []
+
+const numScritti = []
+
+const numTrovati = []
+
+console.log(numCasuali);
+console.log(numScritti);
+console.log(numTrovati);
+
+
+if (numCasuali == numScritti){
+
+    numTrovati.push(foundNum)
+}
+
+
+
 for (let i = 0; i < 5; i++){
     
     const num = Math.floor(Math.random() * 100) + 1;
@@ -21,7 +40,7 @@ for (let i = 0; i < 5; i++){
     
     console.log(num);
     
-    
+    numCasuali.push(num)
 }
 
 
@@ -30,29 +49,42 @@ const time = document.querySelector('h2')
 
 let seconds = 0;
 
+
+// 2 - Da lì parte un timer di 5 secondi. 
+
 let clock = setInterval(() => {
     
     
-    if(seconds == 10){
+    if(seconds == 5){
         
         clearInterval(clock);
+        
+        // 3 - Dopo 10 secondi i numeri scompaiono 
         
         numbers.style.display = 'none';
         
         time.style.display = 'none';
 
 
-        
+
         
         setTimeout(() => {
 
-            for (let i = 0; i < 5; i++){
+            for (let i = 1; i < 6; i++){
                         
-                        const foundNum = Number(prompt('Quali sono i numeri scomparsi?'))
+                        // 4 - e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt(). 
+
+                        const foundNum = Number(prompt(`Qual è il ${i}° numero scomparso?`));
+
+                        console.log(foundNum);
+
+                        numScritti.push(foundNum)
                         
                     }
 
-        }, 1000)    
+        }, 0)    
+
+        
         
         
         
@@ -71,20 +103,11 @@ let clock = setInterval(() => {
 
 
 
+// 5 - Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+if(numCasuali.includes(numScritti)){
+    numTrovati.push(numScritti)
+}
 
 
 
